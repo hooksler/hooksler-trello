3b3b1a296f5ae3b2556f16b3c8949bba38205d3b
Alexey Shcherbakov <schalexey@gmail.com>
2015-06-14 14:34:40 +0500

    refactor card model

--- lib/hooksler/trello/models/card.rb
+++ lib/hooksler/trello/models/card.rb
@@ -4,39 +4,29 @@ module Hooksler
       def dispatch(msg, action, data)
         return super unless data['type'] =~ /^[a-z]*Card/
 
-        name = data['data']['card']['name']
-        board = data['data']['board']['name']
-        scope = 'trello.card'
+        action_data = data['data']
+
+        name = action_data['card']['name']
+        board = action_data['board']['name']
+
+        opts = {scope: 'trello.card', name: name, board: board, user: data['memberCreator']['fullName']}
 
         case action
           when 'copy'
-            I18n.t 'copy',
-                   scope: scope,
-                   name: name,
-                   board: board,
-                   from: data['data']['cardSource']['name']
+            I18n.t 'copy', opts.merge( from: action_data['cardSource']['name'] )
           when 'comment'
-            I18n.t 'comment',
-                   scope: scope,
-                   name: name,
-                   user: data['memberCreator']['fullName']
+            I18n.t 'comment', opts
           when 'create'
-            I18n.t 'create',
-                   scope: scope,
-                   name: name,
-                   board: board
+            I18n.t 'create', opts
           when 'update'
-            if data['data']['old'].key?('idList')
+            if action_data['old'].key?('idList')
               I18n.t 'move_to',
-                     scope: "#{scope}.update",
+                     scope: 'trello.card.update',
                      name: name,
-                     before: data['data']['listBefore']['name'],
-                     after: data['data']['listAfter']['name']
-            elsif data['data']['old'].key?('closed')
-              I18n.t data['data']['card']['closed'] ? 'close' : 'open',
-                     scope: scope,
-                     name: name,
-                     board: board
+                     before: action_data['listBefore']['name'],
+                     after: action_data['listAfter']['name']
+            elsif action_data['old'].key?('closed')
+              I18n.t action_data['card']['closed'] ? 'close' : 'open', opts
             end
         end
       end
